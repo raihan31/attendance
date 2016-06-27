080e9be31b783336c3028a30d8d0e388839ce5fd
th-pias <pias221191@gmail.com>
2016-06-27 17:03:25 +0600

    email confirmation added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626061529) do
+ActiveRecord::Schema.define(version: 20160627105642) do
 
   create_table "attends", force: :cascade do |t|
     t.date     "in_date"
@@ -38,8 +38,13 @@ ActiveRecord::Schema.define(version: 20160626061529) do
     t.datetime "updated_at",                                      null: false
     t.string   "first_name",             limit: 255
     t.string   "last_name",              limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
